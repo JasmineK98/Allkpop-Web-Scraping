38b9698f4badd67451a1865ba771cb377bb325bd
Jasmine Kaur <jasminekaur1998@gmail.com>
2020-02-06 19:04:52 +0000

    need to work on scrape.py still

---
+++ api.py
@@ -0,0 +1,114 @@
+from flask import Flask, request
+import sqlite3
+import json
+from sqlite3 import Error
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/posts')
+def show_all_posts():
+	conn = sqlite3.connect(r"C:\Users\Jasmine Kaur\Documents\WebScrapingProject\articles.db")
+	cur = conn.cursor()
+	
+	cur.execute('select id from articles')
+	allIds = cur.fetchall()
+	print(allIds) # all the post ids 
+
+	post_vote_count = []
+	for i in allIds:
+		query = cur.execute('select count(post_id) from votes where post_id=\'%s\'' % (i[0]))
+		num_of_votes = query.fetchall()[0][0]
+		post_vote_count.append(num_of_votes)
+	
+	print(post_vote_count) # votes for each post id 
+
+	postDictList = []
+	index = 0
+	for post, id in zip(allPosts, allIds):
+		postDict = {}
+		postDict["post_id"] = id[0]
+		postDict["body"] = post[0]
+		postDict["vote_count"] = post_vote_count[index]
+		postDictList.append(postDict)
+		index += 1
+	
+	return "{'posts':" + json.dumps(postDictList) + "}"
+
+@app.route('/login', methods=['POST'])
+def login(username):
+	return 'Ok!'
+
+@app.route('/posts/vote')
+def vote_for_post():
+	post_id = request.args.get('post_id')
+	username = request.args.get('username')
+
+	conn = sqlite3.connect(r"C:\Users\Jasmine Kaur\Documents\WebScrapingProject\articles.db")
+	cur = conn.cursor()
+	cur.execute('create table if not exists votes (post_id, username)')
+	
+	cur.execute('select id from articles')
+	allIds = cur.fetchall()
+
+	# check if valid post id is given
+	extractedIds = []
+	for i in allIds:
+		extractedIds.append(i[0])
+
+	if int(post_id) not in extractedIds:
+		return '{"error": "Invalid post ID"}'
+
+	# check if user has never voted for that post before
+	query = cur.execute('select * from votes where post_id=\'%s\' and username=\'%s\'' % (post_id, username))
+	if (len(query.fetchall()) == 0):
+		voter = (post_id, username)
+		insert_voter(conn, voter)
+		query = cur.execute('select count(post_id) from votes where post_id=\'%s\'' % (post_id))
+		post_vote_count = query.fetchall()[0][0]
+		return "{'post_id': %s, 'vote_count': %s}" % (post_id, post_vote_count) 
+	else:
+		return '{"error": "User has voted for this post before"}'
+
+def insert_voter(conn, voter):
+	cur = conn.cursor()
+	cur.execute('insert into votes(post_id, username) values(?,?)', voter)
+	conn.commit()
+
+@app.route('/posts/top')
+def show_top_posts():
+	count = int(request.args.get('count')) # the number of top posts to display
+	print(type(count))
+	
+	conn = sqlite3.connect(r"C:\Users\Jasmine Kaur\Documents\WebScrapingProject\articles.db")
+	cur = conn.cursor()
+	
+	top_posts = {}
+	cur.execute('select id from articles')
+	allIds = cur.fetchall()
+
+	extractedIds = []
+	for i in allIds:
+		extractedIds.append(i[0])
+
+	for i in extractedIds:
+		query = cur.execute('select count(post_id) from votes where post_id=\'%s\'' % (i))
+		num_of_votes = query.fetchall()[0][0]
+		top_posts[i] =  num_of_votes
+		
+	vote_counts = {k: v for k, v in sorted(top_posts.items(), key=lambda item: item[1], reverse=True)[:count]} # the top n posts where n = count
+	print(vote_counts) # prints the top voted posts 
+
+	postDictList = []
+	for post_id,vote in vote_counts.items():
+		postDict = {}
+		postDict["post_id"] = post_id
+		query = cur.execute('select title from articles where id=\'%s\'' % (post_id))
+		body = query.fetchall()[0][0]
+		postDict["body"] = body
+		postDict["vote_count"] = vote
+		postDictList.append(postDict)
+	
+	return "{'posts':" + json.dumps(postDictList) + "}"
+
+app.run(host = '0.0.0.0')
---
+++ scrape.py
@@ -0,0 +1,87 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd 
+import sqlite3
+from sqlite3 import Error
+import csv
+
+#TODO adding updated articles to the database without duplicates -  gen hashes for body strings
+
+usernames = []
+articleTitles = []
+timesPosted = []
+
+def create_table(cur):
+    """
+    Create the articles table in the database 
+    """
+    try:
+        cur.execute('create table articles (id integer primary key, title, username, time_posted)')
+    except Error as e:
+        print(e)
+
+def create_connection(db_file):
+    """ 
+    Create a database connection to the SQLite database specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        # conn represents the database
+        conn = sqlite3.connect(db_file)
+        cur = conn.cursor()
+        create_table(cur)
+    except Error as e:
+        print(e)
+
+    return conn
+
+def insert_article(conn, article):
+    """
+    Insert a new article into the articles table
+    :param conn:
+    :param article:
+    :return: article id
+    """
+    cur = conn.cursor()
+    cur.execute('insert into articles(title, username, time_posted) values(?,?,?)', article)
+  
+def scrape_articles():
+    url = 'https://www.allkpop.com/'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    
+    startBlock = soup.findAll('div', attrs={'class': 'home_left width73'})[0]
+
+    for a in startBlock.findAll('article', attrs={'class': 'list'}):
+   
+        if len(a.findAll('div', attrs={'class':'title'})) != 0:
+            title = a.findAll('div', attrs={'class':'title'})[0]
+    
+        if len(a.findAll('div', attrs={'class':'info'})) != 0:
+            user = a.findAll('div', attrs={'class':'info'})[0]
+   
+        extractedTitle = title.find('a', href=True, attrs={'class': 'h_a_i'})
+        extractedUser = user.find('span', attrs={'class': 'akp_display_name author'})
+        extractedTime = user.find('span', attrs={'class': 'realtime'})
+
+        articleTitles.append(extractedTitle.text)
+        usernames.append(extractedUser.text)
+        timesPosted.append(extractedTime.text)
+
+def main():
+    scrape_articles()
+  
+    database = r'C:\Users\Jasmine Kaur\Documents\WebScrapingProject\articles.db'
+
+    # Create a database connection
+    conn = create_connection(database)
+    with conn:
+        for i in range(len(articleTitles)):
+            article = (articleTitles[i], usernames[i], timesPosted[i])
+            insert_article(conn, article)
+
+main()
+
+
